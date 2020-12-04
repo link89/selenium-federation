3b689cc42fb0008a459cc655098fc38235644571
Henry Xu <henry.xu@ringcentral.com>
2020-12-04 22:46:05 +0800

    feat: pm2 starter

--- config.ts
+++ config.ts
@@ -3,9 +3,11 @@ import { parse } from 'yaml';
 import fs from 'fs';
 import { configurationSchema } from './schemas';
 
-export const argv = yargs(process.argv.slice(2)).options({
-  c: { type: 'string', demandOption: false },
-}).argv;
+export const argv = yargs(process.argv.slice(2)).
+  usage('start selenium-federation service').
+  options({
+    c: { type: 'string', demandOption: true, description: 'configuration file' },
+  }).argv;
 
 const rawConfig = argv.c ? parse(fs.readFileSync(argv.c, 'utf-8')) : {}
 
