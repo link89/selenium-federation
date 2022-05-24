ee9baea921f8745e9d3584bff28540dce26f9a7a
weihong.xu <xuweihong.cn@gmail.com>
2022-05-24 22:42:58 +0800

    feat: KeywordMutex

--- src/utils.ts
+++ src/utils.ts
@@ -95,10 +95,43 @@ export class LruCache<K, V> {
   }
 }
 
+export class KeywordMutex<K> {
+  private map = new Map<K, Semaphore>();
+
+  constructor() { }
+
+  async lock(key: K) {
+    let sempahore = this.map.get(key);
+    if (!sempahore) {
+      sempahore = new Semaphore(1);
+      this.map.set(key, sempahore);
+    }
+    await sempahore.wait();
+  }
+
+  release(key: K) {
+    const sempahore = this.map.get(key);
+    if (!sempahore) {
+      throw Error(`key: ${key} has not been locked`)
+    }
+    sempahore.signal();
+    if (1 === sempahore.size) {
+      this.map.delete(key);  // remove keyword when lock is free, or else will have memory leak
+    }
+  }
+
+  getPending(key: K) {
+    const sempahore = this.map.get(key);
+    return sempahore ? (1 - sempahore.size) : 0;
+  }
+}
+
+
 export class Semaphore {
   queue: ((value?: any) => void)[] = [];
 
-  constructor(private size: number) { }
+
+  constructor(public size: number) { }
 
   async wait() {
     if (this.size - 1 < 0) {
