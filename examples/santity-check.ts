218a0f0419ed756e4ddfaa6a594d7bb4bb013879
weihong.xu <xuweihong.cn@gmail.com>
2022-06-07 20:44:33 +0800

    feat: set sf:sessionUrl

--- examples/santity-check.ts
+++ examples/santity-check.ts
@@ -17,6 +17,8 @@ void (async () => {
     try {
       const browserName = browser.config.browserName;
       const driver = await remote({ ...opt, capabilities: { browserName, 'sf:browserUUID': browser.config.uuid } });
+      console.log(driver.capabilities);
+
       await driver.setTimeout({pageLoad: 60e3, script: 60e3, implicit: 60e3});
 
       await driver.url(`https://html5test.com/`);
